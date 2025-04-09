822e1d84a2b6db8d30e26adddf92bc6e37e4a853
brenddon11062 <usertest199902@gmail.com>
2025-04-09 20:22:08 +0000

    Adicionado sistema de banco de dados no C

--- src/main.c
+++ src/main.c
@@ -1,8 +1,55 @@
 #include <stdio.h>
 #include "utils/menu.h"
+#include <stdlib.h>
+#include <string.h>
+
+#define ARQUIVO_BANCO "../data/usuarios.txt"
+
 
 int main() {
 
-  menuIteracao();
+  // menuInteracao();
+
+  // id, nome, saldo, limite
+  int id;
+  char nome[100];
+  double saldo; 
+  double limite;
+
+  // printf("Digite seu nome: ");
+
+
+  // captura de dados
+  printf("Digite seu nome: ");
+  fgets(nome, 100, stdin);
+  nome[strcspn(nome, "\n")] = 0;
+
+  // limpar buffer
+  // getchar();
+
+  printf("Digite seu id: ");
+  scanf("%d", &id);
+  printf("Digite seu saldo: ");
+  scanf("%lf", &saldo);
+  printf("Digite seu limite: ");
+  scanf("%lf", &limite);
+
+  printf("Dados do usuário: \n");
+  printf("ID: %d\n", id);
+  printf("Nome: %s\n", nome);
+  printf("Saldo: %.2lf\n", saldo);
+  printf("Limite: %.2lf\n", limite);
+
+  FILE *arquivo = fopen(ARQUIVO_BANCO, "a");
+
+  if (arquivo == NULL) {
+    printf("Erro! Não foi possível abrir o arquivo.\n");
+    return 0;
+}
+  fprintf(arquivo, "%d;%s;%.2lf;%.2lf\n", id, nome, saldo, limite);
+
+  printf("Usuário cadastrado com sucesso.\n\n");
+
   return 0;
+
 }
